6e5cef1ec21b682efa2aa6fd558103f0fea9ce18
The Nasty <tmckeown@gmail.com>
2024-01-05 00:09:42 -0500

    On branch c-#@ce11ddd0d2 You are currently cherry-picking commit 07ccd8e.   (all conflicts fixed: run "git cherry-pick --continue")   (use "git cherry-pick --skip" to skip this patch)   (use "git cherry-pick --abort" to cancel the cherry-pick operation)
    Changes to be committed:
    	modified:   README.md
    	modified:   lib/parse-args.js
    	modified:   package-lock.json
    	modified:   package.json
    	new file:   test/fixtures/config/.c8.config.cjs
    	new file:   test/fixtures/config/.c8.config.js
    	new file:   test/fixtures/config/.c8rc
    	new file:   test/fixtures/config/.c8rc.cjs
    	new file:   test/fixtures/config/.c8rc.js
    	new file:   test/fixtures/config/.c8rc.yaml
    	new file:   test/fixtures/config/.c8rc.yml
    	new file:   test/fixtures/config/.nyc.config.cjs
    	new file:   test/fixtures/config/.nyc.config.js
    	new file:   test/fixtures/config/.nycrc
    	new file:   test/fixtures/config/.nycrc.json
    	new file:   test/fixtures/config/.nycrc.yaml
    	new file:   test/fixtures/config/.nycrc.yml
    	new file:   test/fixtures/config/c8.config.cjs
    	new file:   test/fixtures/config/c8.config.js
    	new file:   test/fixtures/config/nyc.config.cjs
    	new file:   test/fixtures/config/nyc.config.js
    	new file:   test/fixtures/config/snapshots/.c8.config.cjs.snap
    	new file:   test/fixtures/config/snapshots/.c8.config.js.snap
    	new file:   test/fixtures/config/snapshots/.c8rc.cjs.snap
    	new file:   test/fixtures/config/snapshots/.c8rc.js.snap
    	new file:   test/fixtures/config/snapshots/.c8rc.json.snap
    	new file:   test/fixtures/config/snapshots/.c8rc.yaml.snap
    	new file:   test/fixtures/config/snapshots/.c8rc.yml.snap
    	new file:   test/fixtures/config/snapshots/.nyc.config.cjs.snap
    	new file:   test/fixtures/config/snapshots/.nyc.config.js.snap
    	new file:   test/fixtures/config/snapshots/.nycrc.json.snap
    	new file:   test/fixtures/config/snapshots/.nycrc.snap
    	new file:   test/fixtures/config/snapshots/.nycrc.yaml.snap
    	new file:   test/fixtures/config/snapshots/.nycrc.yml.snap
    	new file:   test/fixtures/config/snapshots/c8.config.cjs.snap
    	new file:   test/fixtures/config/snapshots/c8.config.js.snap
    	new file:   test/fixtures/config/snapshots/nyc.config.cjs.snap
    	new file:   test/fixtures/config/snapshots/nyc.config.js.snap
    	modified:   test/integration.js.snap
    	new file:   test/parse-args-helper.js
    	modified:   test/parse-args.js

---
+++ test/parse-args-helper.js
@@ -0,0 +1,55 @@
+/* global it */
+
+const { buildYargs } = require('../lib/parse-args')
+const { spawnSync } = require('child_process')
+const { mkdirSync, copyFileSync, rmSync, existsSync } = require('fs')
+const chaiJestSnapshot = require('chai-jest-snapshot')
+
+const c8Path = require.resolve('../bin/c8')
+const nodePath = process.execPath
+
+const testPath = './test/fixtures/tmp-config-test'
+function testReadingConfigFile (fileNameLineNumberMap, filePath) {
+  Object.keys(fileNameLineNumberMap).forEach((fileName) => {
+    it(`should be able to resolve config file ${fileName} with --config flag`, () => {
+      const expectedLines = fileNameLineNumberMap[fileName]
+      const argv = buildYargs().parse(['node', 'c8', 'my-app', '--config', require.resolve(`${filePath}${fileName}`)])
+      argv.lines.should.be.equal(expectedLines)
+    })
+
+    it(`should be able to resolve config file ${fileName} by detection`, () => {
+      // set the snapshot filename
+      chaiJestSnapshot.setFilename('./test/fixtures/config/snapshots/' + fileName + '.snap')
+
+      copyFileSync('./test/fixtures/config/' + fileName, testPath + '/' + fileName)
+      // Run V8 in the dir above
+      const { output } = spawnSync(nodePath, [
+        c8Path,
+        nodePath,
+        require.resolve('./fixtures/tmp-config-test/normal.js')
+      ])
+      output.toString('utf8').should.matchSnapshot()
+    })
+  })
+}
+
+const beforeTestReadingConfigFile = () => {
+  // make the directory tmp-config-test
+  mkdirSync(testPath)
+
+  // Copy config file in fileName and test/fixtures/normal.js to dir above
+  copyFileSync('./test/fixtures/normal.js', testPath + '/normal.js')
+  copyFileSync('./test/fixtures/async.js', testPath + '/async.js')
+}
+
+const afterTestReadingConfigFile = () => {
+  if (existsSync(testPath)) {
+    rmSync(testPath, { recursive: true, force: true })
+  }
+}
+
+module.exports = {
+  testReadingConfigFile: testReadingConfigFile,
+  beforeTestReadingConfigFile: beforeTestReadingConfigFile,
+  afterTestReadingConfigFile: afterTestReadingConfigFile
+}
