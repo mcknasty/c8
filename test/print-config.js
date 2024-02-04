52cc389eaee4958a65a37032e74fcdae696c6753
Trevor D. McKeown <tmckeown@gmail.com>
2024-02-04 10:27:39 -0500

    refactor: added print-config.js file refactor: moving banner art to a function with a template literal refactoring test/print-config.js unit tests refactor: deprecating chai should statements refactor: require statments order refactor: moved before hook to inside the describe block refactor: changed skip syntax refactor: print-format unit tests to be more concise refactor test/help-message.js refactor: deprecating should statements refactor: requires in the header of the file docs: Adding jdocs styled comments docs: refactor todo notes fix: deprecating replaceAll for replace fix: updating snapshots test: unit test to cover gap in formatPrintVariable function refactor: cleanUpArgumentArray to make logic more concise refactor: addressing a gap in coverage test: adding a unit test for cleanUpArgumentArray docs: updating todo items and noting a gap in coverage refactor: config table generation is now split up into functions refactor,docs: fixed spelling mistakes refactor: restricting the --print-config-format flag to either text or json refactor: changing the name of the interface from cleanUpArgumentArray to cleanUpArgumentObject docs: adding jsdocs to new functions in lib/print-config.js fix: unit test with cross platform path concerns docs: fixing spelling mistakes test: unit test for printConfigText to cover gap docs: doc block for "Test: ensure config prints without a config file" docs: updating the readme to include print config flags refactor: unit test for no config file - compressed snapshot refactor: print config display to add padding refactor: print config to dynamically draw certain lines docs: adding doc blocks to unit test refactor: changing name of test helpers function file refactor: eslint disable lines refactor: eslint disable lines fix: fixing snapshop error by removing dynamic display components test: updating snapshot on windows docs: add todo items, misc notes feat: added longestColumn function test: longestColumn unit test docs: fixed some errors in jsDocs.  Added jsDocs for some functions. fix: Error in text rendering of configuration due to padding bug. refactor: printing of objects and arrays.  left a todo item. fix: test case failing on number of columns in config table test: added a test to printing objects fix: an error where the graphic and summary were not centered

--- test/print-config.js
+++ test/print-config.js
@@ -1,26 +1,32 @@
-/* global describe, before, beforeEach, it */
+/* global describe, before, it */
 
 const { rm } = require('fs')
-const { runSpawn, textGetConfigKey } = require('./print-config-helpers')
-const c8Path = require.resolve('../bin/c8')
 const os = require('os')
-const isWin = (os.platform() === 'win32')
-const OsStr = (isWin) ? 'windows' : 'unix'
+const { expect } = require('chai')
+const { resolve } = require('path')
 const chaiJestSnapshot = require('chai-jest-snapshot')
-const { assert } = require('chai')
 
-require('chai').should()
+const {
+  runSpawn,
+  textGetConfigKey
+} = require('./test-helpers')
+
+const {
+  formatPrintVariable,
+  cleanUpArgumentObject,
+  longestColumn
+} = require('../lib/print-config')
+
+const { buildYargs } = require('../lib/parse-args')
+const c8Path = require.resolve('../bin/c8')
+
 require('chai')
   .use(chaiJestSnapshot)
-  .should()
 
-before(cb => rm('tmp', { recursive: true, force: true }, cb))
-
-beforeEach(function () {
-  chaiJestSnapshot.configureUsingMochaContext(this)
-})
-
+const isWin = (os.platform() === 'win32')
+const OsStr = (isWin) ? 'windows' : 'unix'
 describe(`print derived configuration CLI option - ${OsStr}`, function () {
+  before(cb => rm('tmp', { recursive: true, force: true }, cb))
   /**
    *
    *  Test: Ensure Valid JSON
@@ -34,9 +40,9 @@ describe(`print derived configuration CLI option - ${OsStr}`, function () {
 
     try {
       const out = runSpawn([c8Path, '--print-config', '--print-config-format=json'])
-      out.should.matchSnapshot()
+      expect(out).to.matchSnapshot()
     } catch (e) {
-      assert.fail('invalid json document produced from --print-config option')
+      expect.fail('invalid json document produced from --print-config option')
     }
   })
 
@@ -45,11 +51,10 @@ describe(`print derived configuration CLI option - ${OsStr}`, function () {
    *  Test: Ensure comma delimited values transform into an array
    *  Command: C8 --reporter=lcov,text --print-config --print-config-format=json
    *
-   *  Todo:  There is a bug in yargs where this is not transformedd into an array
+   *  Todo:  There is a bug in yargs where this is not transformed into an array
    *   Skipping test for now
    */
-  it('ensure comma delimited values transform into an array', function () {
-    this.skip()
+  it.skip('ensure comma delimited values transform into an array', function () {
     const out = runSpawn([
       c8Path,
       '--reporter=lcov,text',
@@ -57,10 +62,8 @@ describe(`print derived configuration CLI option - ${OsStr}`, function () {
       '--print-config-format=json'
     ])
 
-    const includesKey = Object.keys(out).includes('reporter')
-    const checkFor = ['lcov', 'text']
-    includesKey.should.eql(true)
-    out.reporter.should.eql(checkFor)
+    expect(Object.keys(out).includes('reporter')).to.equal(true)
+    expect(out.reporter).to.eql(['lcov', 'text'])
   })
 
   /**
@@ -72,15 +75,13 @@ describe(`print derived configuration CLI option - ${OsStr}`, function () {
   it('ensure default project configuration file is loaded', function () {
     const out = runSpawn([c8Path, '--print-config', '--print-config-format=json'])
 
-    const includesKey = Object.keys(out).includes('config')
-    includesKey.should.eql(true)
+    expect(Object.keys(out).includes('config')).to.equal(true)
     out.config.endsWith('.nycrc')
   })
 
   ;['text', 'json'].forEach((format) => {
     describe(`${format} format option`, function () {
-      // Can I shorten this line?
-      const textParam = (format === 'text')
+      const textParam = format === 'text'
 
       /**
        *
@@ -90,25 +91,29 @@ describe(`print derived configuration CLI option - ${OsStr}`, function () {
        */
       it('ensure loads config file from cli', function () {
         // Can I shorten this line?
+        const configFile = './test/fixtures/config/.c8rc.json'
         const out = runSpawn([
           c8Path,
-          '--config=./test/fixtures/config/.c8rc.json',
+          `--config=${configFile}`,
           '--print-config',
           `--print-config-format=${format}`
         ], textParam)
 
         if (format === 'json') {
-          const includesKey = Object.keys(out).includes('config')
-          includesKey.should.eql(true)
-          out.config.should.eql('./test/fixtures/config/.c8rc.json')
-        } else if (format === 'text') {
-          const value = textGetConfigKey(out, 'config')
-          if (value) {
-            value.should.eql('./test/fixtures/config/.c8rc.json')
-          } else {
-            assert.fail('couldn\'t find configuration value for option --config')
-          }
+          expect(Object.keys(out)
+            .includes('config')).to.equal(true)
         }
+
+        const value = (format === 'json')
+          ? out.config
+          : textGetConfigKey(out, 'config')
+
+        if (!value) {
+          expect
+            .fail('couldn\'t find configuration value for option --config')
+        }
+
+        expect(value).to.eql(configFile)
       })
 
       /**
@@ -126,19 +131,143 @@ describe(`print derived configuration CLI option - ${OsStr}`, function () {
         ], textParam)
 
         if (format === 'json') {
-          const includesKey = Object.keys(out).includes('reporter')
-          includesKey.should.eql(true)
-          out.reporter.should.eql('lcov')
-        } else if (format === 'text') {
-          const value = textGetConfigKey(out, 'reporter')
-          if (value) {
-            // Todo: when the load comma delimited text array bug is fixed, need to adjust this line
-            value.should.eql('lcov')
-          } else {
-            assert.fail('couldn\'t find configuration value for option --reporter')
-          }
+          expect(Object.keys(out)
+            .includes('reporter')).to.equal(true)
         }
+
+        const value = (format === 'json')
+          ? out.reporter
+          : textGetConfigKey(out, 'reporter')
+
+        if (!value) {
+          expect
+            .fail('couldn\'t find configuration value for option --reporter')
+        }
+
+        // Todo: when the load comma delimited text array bug is fixed, need to adjust this line
+        expect(value).to.eql('lcov')
       })
     })
+  })
+
+  /**
+   * Test: ensure objects can be printed in derived config display
+   *
+   *  a unit test to ensure coverage of printed objects
+   */
+  it('ensure objects can be printed in derived config display', function () {
+    let testObject = {
+      a: 'str1',
+      b: 4,
+      c: false,
+      d: undefined,
+      e: null,
+      f: ['one', 'two', 'three'],
+      g: {
+        five: 'six',
+        seven: false,
+        eight: [
+          {
+            nine: 9,
+            ten: '10'
+          },
+          {
+            eleven: true,
+            twelve: null
+          }
+        ]
+      }
+    }
+
+    let output = formatPrintVariable(testObject, '').replace(/\s+/g, '')
+    let expected = '{"a":"str1","b":4,"c":false,"e":null,"f":["one","two","three"],' +
+      '"g":{"five":"six","seven":false,"eight":[{"nine":9,"ten":"10"},' +
+      '{"eleven":true,"twelve":null}]}}'
+
+    expect(output).to.equal(expected)
+
+    testObject = {}
+    output = formatPrintVariable(testObject, '').replace(/\s+/g, '')
+    expected = '{}'
+    expect(output).to.equal(expected)
+  })
+
+  /**
+   * Test: testing cleanUpArgumentObject function
+   *
+   * Just a unit test that helped develop the function
+   */
+  it('testing cleanUpArgumentObject function', function () {
+    const args = Object.freeze([
+      'node',
+      'c8',
+      '--print-config',
+      '--lines',
+      '100',
+      '--config',
+      require.resolve('./fixtures/config/.c8rc.json')
+    ])
+    const argsv = buildYargs().parse(args)
+    const cleanArgs = cleanUpArgumentObject(argsv)
+
+    const configPath = resolve('./test/fixtures/config/.c8rc.json')
+
+    expect(cleanArgs.config).to.equal(configPath)
+
+    const noCamelCaseKeys = Object.keys(cleanArgs)
+      .map(v => [...v.matchAll(/([A-Z])/g)].length === 0)
+      .reduce((prev, curr) => prev && curr)
+
+    expect(noCamelCaseKeys).to.eql(true)
+  })
+
+  /**
+   * Test: ensure config prints without a config file
+   *
+   * Run the printConfigText function with an empty string
+   * assigned to the config key and expect the configuration
+   * to still print.
+   *
+   */
+  it('ensure config prints without a config file', function () {
+    chaiJestSnapshot.setTestName('ensure config prints without a config file')
+    chaiJestSnapshot.setFilename(`./test/print-config-${OsStr}.js.snap`)
+
+    const args = Object.freeze([
+      c8Path,
+      '--config=""',
+      '--print-config',
+      '--lines',
+      '100'
+    ])
+
+    // run the process, get the output and remove
+    // items that are dynamically formatted.
+    const output = runSpawn(args, true, true)
+      .replace(/-{3,}/g, '')
+
+    expect(output).to.matchSnapshot()
+  })
+
+  /**
+   * Test: ensure longestColumn can be passed an array
+   *
+   */
+  it('ensure longestColumn can be passed an array', function () {
+    const args = Object.freeze([
+      c8Path,
+      '--config=""',
+      '--print-config',
+      '--lines',
+      '100'
+    ])
+
+    // run the process, get the output and remove
+    // items that are dynamically formatted.
+    const output = runSpawn(args, true, false)
+      .replace(/-{3,}/g, '')
+
+    const width = longestColumn(output.split('\n'))
+    expect(width).to.be.gte(62)
   })
 })
---
+++ test/test-helpers.js
@@ -0,0 +1,135 @@
+const { spawnSync } = require('child_process')
+const os = require('os')
+
+const nodePath = process.execPath
+const isWin = (os.platform() === 'win32')
+const pwd = process.cwd()
+const whiteSpaceReg = /[\\\s]/g
+const pwdReg = new RegExp(pwd, 'g')
+
+/**
+ * Function: runSpawn
+ *
+ * @param {Array} args: array of arguments to pass
+ *  to child spawned process.
+ * @param {Boolean} text=false: expect json or cli
+ *  text to be returned.
+ * @param {Boolean} stripWhiteSpace=false: should all
+ *  whitespace be removed from the `out` string?
+ * @returns {String} out: a string representing the stdout
+ *   of the child process
+ *
+ * A wrapper function around spawnSync.
+ * Node.js Docs: http://tinyurl.com/66ztyx9b
+ *
+ * Todo:  Whitespace for snapshots is showing up in
+ * different amounts on different os platforms.
+ *
+ * I am concern about this issue in the chai-snapshot
+ * package.
+ *
+ * Issue described in jest
+ * https://github.com/jestjs/jest/pull/9203
+ *
+ * Last time chai-jest-snapshot was publish was 6
+ * years ago.
+ *
+ * https://www.npmjs.com/package/chai-jest-snapshot?activeTab=versions
+ *
+ * Alternative packages that might work.
+ *
+ * https://www.npmjs.com/package/mocha-chai-jest-snapshot
+ *
+ * https://www.npmjs.com/package/chai-snapshot-matcher/v/2.0.2
+ *
+ *
+ * Another issue I am seeing is that all output is enclosed in
+ * "," characters.  I addressed this in the json output, but
+ * it is still present in the text snapshots
+ */
+const runSpawn = (args, text = false, stripWhiteSpace = false) => {
+  const doubleQuoteReg = /"/g
+  const slashReg = /\\/g
+
+  const { output } = spawnSync(nodePath, args)
+
+  let out = output.toString('utf8')
+
+  if (isWin && text) {
+    const jsonEncodedPwd = JSON.stringify(pwd)
+      .replace(doubleQuoteReg, '')
+    const encodedPwdRegEx = new RegExp(jsonEncodedPwd, 'g')
+    out = out.replace(encodedPwdRegEx, '.')
+  } else if (isWin && !text) {
+    const jsonEncodedPwd = JSON.stringify(pwd)
+      .replace(doubleQuoteReg, '')
+      .replace(slashReg, '\\\\')
+    const encodedPwdRegEx = new RegExp(jsonEncodedPwd, 'g')
+    out = out.replace(encodedPwdRegEx, '.')
+  } else if (!isWin) {
+    out = out.replace(pwdReg, '.')
+  }
+
+  // Todo: For certain cases we need to strip out all whitespace in
+  // snapshots. See notes above
+  if (text && stripWhiteSpace) {
+    out = out.replace(whiteSpaceReg, '')
+  }
+
+  if (!text) {
+    out = cleanJson(out)
+  }
+
+  return out
+}
+
+/**
+ * Function: cleanJson
+ *
+ * @param {String} out: string of json to scrub.
+ * @returns {String} - valid json object.
+ *
+ * JSON needs to get scrubbed from additional characters
+ * when being read from SpawnSync.
+ *
+ */
+const cleanJson = (out) => {
+  const o = out.substring(1)
+    .substring(0, out.length - 2)
+    .replace(pwdReg, '.')
+
+  return JSON.parse(o)
+}
+
+/**
+ * Function: textGetConfigKey
+ *
+ * @param {String} out: utf-8 formatted output from
+ *  child process of c8 with --print-config flag.
+ * @param {String} key: of configuration setting
+ *  to return.
+ * @returns {String|null}: value of key, if
+ *  found or null.
+ *
+ * Get a value of a configuration key from text input.
+ *
+ */
+const textGetConfigKey = (out, key) => {
+  const newLineReturn = (isWin) ? '\r\n' : '\n'
+  const newLineRegEx = new RegExp(newLineReturn, 'g')
+
+  let value = null
+  const keyReg = new RegExp(key + ':.+', 'g')
+  const matches = [...out.matchAll(keyReg)]
+  if (matches.length > 0 && typeof matches[0][0] === 'string') {
+    const fileName = matches[0][0].replace(newLineRegEx, '')
+      .replace(key + ':', '')
+      .replace(whiteSpaceReg, '')
+      .replace(/'/g, '')
+    value = fileName
+  }
+
+  return value
+}
+
+module.exports = { runSpawn, cleanJson, textGetConfigKey }
